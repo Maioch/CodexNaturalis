523c7dc5c229478e6b2bbe65e0eb8f33a1a4d51f
topox <taxom2005@gmail.com>
2024-06-12 10:17:18 +0200

    Added the ReconnectableView interface.

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/ReconnectableView.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.view;
+
+/**
+ * Interface representing the view classes that implement reconnection.
+ */
+public interface ReconnectableView {
+    void showDisconnectionMessage();
+}
