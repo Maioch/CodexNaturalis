a4b5bd958ada2c73416ead86deafc58ea041868b
topox <taxom2005@gmail.com>
2024-06-26 21:39:19 +0200

    Fully moved ping handling to ExchangeHandlerManager, changed the level of certain logs, polished the documentation for some classes.

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/gui/controllers/MatchBrowserViewController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/gui/controllers/MatchBrowserViewController.java
@@ -12,7 +12,6 @@ import it.polimi.ingsw.network.shared.messages.setup.NewGameMessage;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 
